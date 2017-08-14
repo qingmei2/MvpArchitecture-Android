7b026af2124f35a726ceae329ff44b9ab45941ee
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-08-14 15:07:44 +0800

    Add:normal code commit.

---
+++ app/src/main/java/com/qingmei2/mvparchitecture/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.qingmei2.mvparchitecture;
+
+import com.qingmei2.module.base.BaseActivity;
+import com.qingmei2.module.di.component.AppComponent;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+}
