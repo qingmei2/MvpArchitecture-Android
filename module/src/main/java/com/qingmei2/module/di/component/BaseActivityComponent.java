782c1c5dfb74eb9befce3332ee3432e768dc9a34
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-09-22 15:58:24 +0800

    Remove the error annotations.

--- module/src/main/java/com/qingmei2/module/di/component/BaseActivityComponent.java
+++ module/src/main/java/com/qingmei2/module/di/component/BaseActivityComponent.java
@@ -1,7 +1,6 @@
 package com.qingmei2.module.di.component;
 
 import com.qingmei2.module.base.BaseActivity;
-import com.qingmei2.module.di.module.activity.HomeActivityModule;
 
 import dagger.Subcomponent;
 import dagger.android.AndroidInjectionModule;
@@ -13,8 +12,6 @@ import dagger.android.AndroidInjector;
  */
 @Subcomponent(modules = {
         AndroidInjectionModule.class,
-        //每添加一个ActivityModule,只需要在此处添加对应的Module依赖即可
-        HomeActivityModule.class
 })
 public interface BaseActivityComponent extends AndroidInjector<BaseActivity> {
 
